d4dd8b9be6d572d4f3769b8547f4b84780c53f04
Noel Cortes <noelcortes472640@gmail.com>
2019-03-05 22:35:34 -0500

    Make file and function declarations in .h

--- main.c
+++ main.c
@@ -6,12 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-void populateInsert(char total[], char t1[], char t2[], char t3[], char t4[], int *elaborate, int *incorrectFormat);
-void populate(char total[], char t1[], char t2[], char t3[], char t4[], char t5[], char t6[]);
-void ins(char dml[], char table[], char column[], char data[], int *elaborate);
-void upd(char dml[], char table[], char column[], char data[], char refColumn[], char refData[]);
-void del(char dml[], char table[], char column[], char data[]);
+#include "declarations.h"
 
 int main()
 {
